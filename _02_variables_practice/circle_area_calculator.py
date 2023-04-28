5aee96bbb1437e1419bf1e688ef760d9f621183c
jereycui2008 <jeremycuid@gmail.com>
2023-04-28 16:58:17 -0700

    hi:)hihihihihihihihi

--- _02_variables_practice/circle_area_calculator.py
+++ _02_variables_practice/circle_area_calculator.py
@@ -13,22 +13,24 @@ if __name__ == '__main__':
     
     # Ask the user for the radius in pixels and store it in a variable
     # simpledialog.askinteger()
-    
+    number = simpledialog.askinteger(None, prompt="give me number")
     # Make a new turtle
-    
+    bob = turtle.Turtle()
     # Have your turtle draw a circle with the correct radius
     # my_turtle.circle()
-
+    bob.circle(radius=number)
     # Call the turtle .penup() method
-
+    bob.penup()
     # Move your turtle to a new x,y position using .goto()
-
+    bob.goto(5, 3)
     # Calculate the area of your circle and store it in a variable
     # Hint, you can use math.pi
-    
+    area = math.pi*(number**2)
     # Write the area of your circle using the turtle .write() method
-    # my_turtle.write(arg="area = " + str(area), move=True, align='left', font=('Arial',8,'normal'))
+
+    bob.write(arg="area = " + str(area), move=True, align='left', font=('Arial',12,'normal'))
 
     # Hide your turtle
-
+    bob.hideturtle()
     # Call turtle.done()
+    turtle.done()
